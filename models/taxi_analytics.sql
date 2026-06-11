41ea3b6c6c4fe37a2e3e23f9b999f54bce7d4b2d
maddyrondakrish@gmail.com <maddyrondakrish@gmail.com>
2026-06-11 09:13:49 +0000

    Added fare_per_mile metric and model documentation

--- models/taxi_analytics.sql
+++ models/taxi_analytics.sql
@@ -1,36 +1,28 @@
--- Create transformed taxi dataset
-{{ config(materialized='table') }}
 SELECT
 
-    -- Vendor information
     VendorID,
 
-    -- Pickup and dropoff times
     tpep_pickup_datetime,
     tpep_dropoff_datetime,
 
-    -- Extract trip date
     DATE(tpep_pickup_datetime) AS trip_date,
 
-    -- Extract pickup hour
     EXTRACT(HOUR FROM tpep_pickup_datetime) AS pickup_hour,
 
-    -- Calculate trip duration in minutes
     TIMESTAMP_DIFF(
         tpep_dropoff_datetime,
         tpep_pickup_datetime,
         MINUTE
     ) AS trip_duration_minutes,
 
-    -- Passenger information
     passenger_count,
 
-    -- Distance
     trip_distance,
 
-    -- Fare details
     fare_amount,
     tip_amount,
-    total_amount
+    total_amount,
+
+    SAFE_DIVIDE(total_amount, trip_distance) AS fare_per_mile
 
 FROM {{ ref('stg_taxi_trips') }}
